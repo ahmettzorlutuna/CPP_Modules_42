39fa707b7d8672f473d49ca8b34c8b7ac871975a
ahmettzorlutuna <ahmettayyip3439@gmail.com>
2026-03-12 00:11:35 +0300

    MyVeryVeryHandsomeMaxEmilianVerstappenOutOfRangeExceptionClassThatNeverExistBeforeLetsGetItSimplyLovely

--- Module07/ex02/includes/Array.hpp
+++ Module07/ex02/includes/Array.hpp
@@ -10,4 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstddef>
+#include <exception>
 
+template <typename T>
+class Array
+{
+    public:
+        Array() : _size(0), _elements(NULL) {};
+        ~Array(){
+            delete[] _elements;
+        };
+        Array(const Array& other){
+            _size = other._size;
+            _elements = new T[_size];
+            for (unsigned int i = 0; i < _size; i++){
+                _elements[i] = other._elements[i];
+            }
+        };
+        Array& operator=(const Array& other){
+            if(this != &other)
+            {
+                if(_elements)
+                    delete[] _elements;
+                _size = other._size;
+                if(_size > 0)
+                {
+                    _elements = new T[_size];
+                    for (unsigned int i = 0; i < _size; i++)
+                        _elements[i] = other._elements[i];
+                }else{
+                    _elements = NULL;
+                }
+            }
+            return *this;
+        };
+        Array(unsigned int n) : _size(n), _elements(new T[n]()) {};
+        class MyVeryVeryHandsomeMaxEmilianVerstappenOutOfRangeExceptionClassThatNeverExistBeforeLetsGetItSimplyLovely: public std::exception
+        {
+            public:
+                virtual const char* what() const throw()
+                {
+                    return "Error:  Index out of bounds!";
+                }
+        };
+        T& operator[](unsigned int index){
+            if(index >= _size || _elements == NULL)
+                throw MyVeryVeryHandsomeMaxEmilianVerstappenOutOfRangeExceptionClassThatNeverExistBeforeLetsGetItSimplyLovely();
+            return _elements[index];
+        }
+        const T& operator[](unsigned int index) const
+        {
+            if(index >= _size || _elements == NULL)
+                throw MyVeryVeryHandsomeMaxEmilianVerstappenOutOfRangeExceptionClassThatNeverExistBeforeLetsGetItSimplyLovely();
+            return _elements[index];
+        }
+        unsigned int size() const{
+            return _size;
+        }
+    private:
+        unsigned int _size;
+        T* _elements;
+};
--- Module07/ex02/srcs/main.cpp
+++ Module07/ex02/srcs/main.cpp
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Array.hpp"
 
+#include <iostream>
+
+int main(void)
+{
+    Array<int> empty;
+    std::cout << empty.size() << std::endl;
+    
+    Array<int> numbers(5);
+    std::cout << numbers.size() << std::endl;
+
+    for (unsigned int i = 0; i < numbers.size(); i++) {
+        numbers[i] = i * 2;
+        std::cout << "numbers[" << i << "] = " << numbers[i] << std::endl;
+    }
+
+    Array<int> copy = numbers;
+
+    std::cout << "Original[0]: " << numbers[0] << std::endl;
+    std::cout << "Copy[0]: " << copy[0] << std::endl;
+
+    numbers[0] = 777;
+
+    std::cout << "Original[0]: " << numbers[0] << std::endl;
+    std::cout << "Copy[0]: " << copy[0] << std::endl;
+
+    try {
+        std::cout << numbers[10] << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
